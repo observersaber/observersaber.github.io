c3dcc9670fffe9aa9d566142996a7d1cd035770f
observer <observersaber@gmail.com>
2020-06-09 16:10:37 +0800

    yee

--- ranje.js
+++ ranje.js
@@ -2,6 +2,24 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("yee");
 output.innerHTML = slider.value;
 
-slider.oninput = function () {
-    output.innerHTML = this.value;
+// slider.oninput = function () {
+//     output.innerHTML = this.value;
+// }
+// slider.onchange = function () {
+//     output.innerHTML = this.value;
+// }
+
+slider.addEventListener("input", function () {
+    listener();
+    slider.addEventListener("change", listener);
+});
+slider.addEventListener("change", function () {
+    listener();
+    slider.removeEventListener("input", listener);
+}); 
+
+function listener() {
+    output.innerHTML = slider.value;
+    document.getElementById("demo").style.fontSize=slider.value+"px";
+    
 }
